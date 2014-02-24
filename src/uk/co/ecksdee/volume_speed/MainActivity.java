4e1c6a26dd92df603edb9ec2d8dacd893292669b
Dave Stevens <dave@crowdlab.com>
2014-02-24 12:38:42 +0000

    Include preferences view.

--- src/uk/co/ecksdee/volume_speed/MainActivity.java
+++ src/uk/co/ecksdee/volume_speed/MainActivity.java
@@ -1,13 +1,17 @@
 package uk.co.ecksdee.volume_speed;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
@@ -19,4 +23,15 @@ public class MainActivity extends Activity {
     return true;
   }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case R.id.action_settings:
+      Intent intent = new Intent(this, SettingsActivity.class);
+      startActivity(intent);
+      return true;
+    default:
+      return false;
+    }
+  }
 }
