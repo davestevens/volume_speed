0f08f916e9baa8554091eadaad9dbd2d213972c5
Dave Stevens <dave@crowdlab.com>
2014-02-24 15:05:15 +0000

    Create methods for updating views.

--- src/uk/co/ecksdee/volume_speed/MainActivity.java
+++ src/uk/co/ecksdee/volume_speed/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends Activity {
   private Speed speed;
   private Audio audio;
   
+  private float previous_speed;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -60,22 +62,34 @@ public class MainActivity extends Activity {
         Integer.parseInt(getString(R.string.pref_volume_steps_default))));
     
     set_volume_bar(audio.volume_percentage());
+    set_status(getString(R.string.initialized));
+    
     // TODO: on change of Preferences, update view
   }
   
-  private void set_volume_bar(Integer v) {
-    ProgressBar volume = (ProgressBar) findViewById(R.id.volume_bar);
-    volume.setProgress(v);
+  private void set_volume_bar(Integer volume) {
+    ProgressBar volume_bar = (ProgressBar) findViewById(R.id.volume_bar);
+    volume_bar.setProgress(volume);
+  }
+  
+  private void set_current_speed(Float speed) {
+    TextView current_speed = (TextView) findViewById(R.id.current_speed);
+    current_speed.setText(speed.toString());
+  }
+  
+  private void set_status(String string) {
+    TextView status = (TextView) findViewById(R.id.status);
+    status.setText(string);
+  }
+  
+  private void set_speed_units(String string) {
+    TextView speed_units = (TextView) findViewById(R.id.speed_units);
+    speed_units.setText(string);
   }
   
   public void initialize_view() {
-    TextView speed_units, status;
-    
-    status = (TextView) findViewById(R.id.status);
-    status.setText(getString(R.string.initializing));
-    
-    speed_units = (TextView) findViewById(R.id.speed_units);
-    speed_units.setText(prefs.getString("pref_speed_units",
+    set_status(getString(R.string.initializing));
+    set_speed_units(prefs.getString("pref_speed_units",
         getString(R.string.pref_speed_units_default)));
   }
   
@@ -85,10 +99,10 @@ public class MainActivity extends Activity {
   }
   
   public void change_in_speed(float speed) {
-    // Update here
     // TODO: convert to correct speed units
     // TODO: check if it has changed passed a threshold
-    Log.i(TAG, "Change in speed: " + speed);
+    set_current_speed(speed);
     // TODO: save speed
+    previous_speed = speed;
   }
 }
